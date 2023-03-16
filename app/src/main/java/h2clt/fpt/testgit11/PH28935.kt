4b57b7b7c4003da8bb173a5bf4b6780f1cddc744
DELL <hungpnph28935@fpt.edu.vn>
2023-03-16 13:44:34 +0700

    test1

---
+++ app/src/main/java/h2clt/fpt/testgit11/PH28935.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.testgit11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PH28935 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ph28935)
+    }
+}
