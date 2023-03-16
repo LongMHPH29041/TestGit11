1e827e2eef366a954db1313d7631f08e03cd52ba
Cuong <cuongncph28957@fpt.edu.vn>
2023-03-16 14:32:16 +0700

    PH28957ddd

--- app/src/main/java/h2clt/fpt/testgit11/cuong.kt
+++ app/src/main/java/h2clt/fpt/testgit11/cuong.kt
@@ -7,5 +7,8 @@ class cuong : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cuong)
+
+
+        ////lllllll
     }
 }
